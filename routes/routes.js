4e50154f48a7cdc733ace9e921ca277d3b1f9627
isabel-gwara <isabel.gwara@gmail.com>
2023-04-06 15:20:48 -0700

    Add routes to homepage

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,19 @@ const scamsRoutes = require("./scams")
 const appRouter = (app, fs) => {
 
     app.get('/', (req, res) => {
-        res.send("Welcome to crypto scams server!")
+        content = `
+        <h1>Crypto Scams from CryptoScamDB</h1>
+        <p>This server uses open source data from the CryptoScamDB to serve data about individual scams.</p>
+        
+        <h1>Routes</h1>
+        
+        <ul>
+            <li> /scams returns all scams from the CryptoScamDB blacklist </li>
+            <li> /scams/:id returns the information from a single scam with the corresponding position in the list of scams </li>
+        </ul>
+        
+        `
+        res.send(content)
     })
 
     scamsRoutes(app, fs)
